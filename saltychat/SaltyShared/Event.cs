75951b5715bd571e56c7433f3b7ed94f808f9be6
Timo Pollmeier (Hyp3rX) <timo.p@cryz-games.de>
2020-06-26 00:45:38 +0200

    Change const propertys values to nameof(propertyname).

--- saltychat/SaltyShared/Event.cs
+++ saltychat/SaltyShared/Event.cs
@@ -1,42 +1,43 @@
-﻿namespace SaltyShared
+﻿// ReSharper disable InconsistentNaming
+namespace SaltyShared
 {
-    public class Event
+    public static class Event
     {
         #region Plugin
-        public const string SaltyChat_Initialize = "SaltyChat_Initialize";
-        public const string SaltyChat_CheckVersion = "SaltyChat_CheckVersion";
-        public const string SaltyChat_SyncClients = "SaltyChat_SyncClients";
-        public const string SaltyChat_UpdateClient = "SaltyChat_UpdateClient";
-        public const string SaltyChat_UpdateVoiceRange = "SaltyChat_UpdateVoiceRange";
-        public const string SaltyChat_UpdateAlive = "SaltyChat_UpdateAlive";
-        public const string SaltyChat_RemoveClient = "SaltyChat_RemoveClient";
+        public const string SaltyChat_Initialize = nameof(SaltyChat_Initialize);
+        public const string SaltyChat_CheckVersion = nameof(SaltyChat_CheckVersion);
+        public const string SaltyChat_SyncClients = nameof(SaltyChat_SyncClients);
+        public const string SaltyChat_UpdateClient = nameof(SaltyChat_UpdateClient);
+        public const string SaltyChat_UpdateVoiceRange = nameof(SaltyChat_UpdateVoiceRange);
+        public const string SaltyChat_UpdateAlive = nameof(SaltyChat_UpdateAlive);
+        public const string SaltyChat_RemoveClient = nameof(SaltyChat_RemoveClient);
         #endregion
 
         #region State Change
-        public const string SaltyChat_TalkStateChanged = "SaltyChat_TalkStateChanged";
-        public const string SaltyChat_MicStateChanged = "SaltyChat_MicStateChanged";
-        public const string SaltyChat_SoundStateChanged = "SaltyChat_SoundStateChanged";
+        public const string SaltyChat_TalkStateChanged = nameof(SaltyChat_TalkStateChanged);
+        public const string SaltyChat_MicStateChanged = nameof(SaltyChat_MicStateChanged);
+        public const string SaltyChat_SoundStateChanged = nameof(SaltyChat_SoundStateChanged);
         #endregion
 
         #region Proximity
-        public const string SaltyChat_SetVoiceRange = "SaltyChat_SetVoiceRange";
+        public const string SaltyChat_SetVoiceRange = nameof(SaltyChat_SetVoiceRange);
         #endregion
 
         #region Phone
-        public const string SaltyChat_EstablishCall = "SaltyChat_EstablishCall";
-        public const string SaltyChat_EstablishCallRelayed = "SaltyChat_EstablishCallRelayed";
-        public const string SaltyChat_EndCall = "SaltyChat_EndCall";
+        public const string SaltyChat_EstablishCall = nameof(SaltyChat_EstablishCall);
+        public const string SaltyChat_EstablishCallRelayed = nameof(SaltyChat_EstablishCallRelayed);
+        public const string SaltyChat_EndCall = nameof(SaltyChat_EndCall);
         #endregion
 
         #region Radio
-        public const string SaltyChat_IsSending = "SaltyChat_IsSending";
-        public const string SaltyChat_IsSendingRelayed = "SaltyChat_IsSendingRelayed";
-        public const string SaltyChat_SetRadioChannel = "SaltyChat_SetRadioChannel";
-        public const string SaltyChat_UpdateRadioTowers = "SaltyChat_UpdateRadioTowers";
+        public const string SaltyChat_IsSending = nameof(SaltyChat_IsSending);
+        public const string SaltyChat_IsSendingRelayed = nameof(SaltyChat_IsSendingRelayed);
+        public const string SaltyChat_SetRadioChannel = nameof(SaltyChat_SetRadioChannel);
+        public const string SaltyChat_UpdateRadioTowers = nameof(SaltyChat_UpdateRadioTowers);
         #endregion
 
         #region Megaphone
-        public const string SaltyChat_IsUsingMegaphone = "SaltyChat_IsUsingMegaphone ";
+        public const string SaltyChat_IsUsingMegaphone = nameof(SaltyChat_IsUsingMegaphone);
         #endregion
     }
 }
