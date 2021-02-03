5e5925f5b4774c4ce1e38dd07ee6af254f0617b0
BlackFlash5 <BlackFlash5@users.noreply.github.com>
2021-02-03 15:32:11 +0100

    talkie(client): added fxmanifest

---
+++ saltytalkie/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page 'NUI/index.html'
+
+dependency 'saltychat'
+
+client_scripts {
+    'SaltyTalkieClient/bin/Debug/SaltyTalkieClient.net.dll'
+}
+
+files {
+    'NUI/*',
+    'Newtonsoft.Json.dll'
+}
