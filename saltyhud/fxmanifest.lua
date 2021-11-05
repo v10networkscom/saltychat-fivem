632bec0fcfdde2a43c1d956313d0c27a9170fa7d
BlackFlash5 <BlackFlash5@users.noreply.github.com>
2021-11-06 00:50:00 +0100

    fix(hud): capitalized NUI path

--- saltyhud/fxmanifest.lua
+++ saltyhud/fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'adamant'
 game 'gta5'
 
-ui_page 'nui/hud.html'
+ui_page 'NUI/hud.html'
 
 client_script 'SaltyClient/bin/Debug/SaltyClient.net.dll'
 
 files {
     'SaltyClient/bin/Debug/SaltyClient.net.pdb',
-    'nui/*',
+    'NUI/*',
     'Newtonsoft.Json.dll',
     'config.json'
 }
